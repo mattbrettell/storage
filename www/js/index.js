f5096e3bd193c3618543bb32ead487ea0c88ef52
mattbrettell <BREM1_15@uni.worc.ac.uk>
2016-10-20 15:39:02 +0100

    cmon

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,6 @@
         //EVENT LISTENER HERE
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("hi matt");
-    }
 
 var key="pen";
 var value="blue";
@@ -11,7 +10,6 @@ window.localStorage.setItem(key,value);
 var key="pen";
 var value=window.localStorage.getItem(key);
 
- {
 		document.addEventListener("resume", onResume, false);
 		document.addEventListener("pause", onPause, false);
 		
