62b6d84c6f0e5dde5f60568c49ca9333276e0508
mattbrettell <BREM1_15@uni.worc.ac.uk>
2016-10-20 15:25:59 +0100

    please

--- www/js/index.js
+++ www/js/index.js
@@ -3,14 +3,14 @@
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("hi matt");
     }
-{
+
 var key="pen";
 var value="blue";
 window.localStorage.setItem(key,value);
 
 var key="pen";
 var value=window.localStorage.getItem(key);
-}
+
  {
 		document.addEventListener("resume", onResume, false);
 		document.addEventListener("pause", onPause, false);
